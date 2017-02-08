0c027a6a808db7f6e8ec6be625776992a21c2db6
Grey Patterson <greypatterson@gmail.com>
2017-02-08 11:00:30 -0800

    Initial commit, I guess

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayField: UILabel!
+    
+    var userWasTyping = false
+    
+    @IBAction func digitPress(_ sender: UIButton) {
+        let digitPressed = sender.title(for: .normal)!
+        if !userWasTyping{
+            userWasTyping = true
+            displayField.text = digitPressed
+        }else{
+            displayField.text = displayField.text! + digitPressed
+        }
+    }
+    @IBAction func deleteKeyPress(_ sender: UIButton) {
+        let length = displayField.text!.characters.count
+        print(length)
+        guard length > 0 else {
+            return
+        }
+        displayField.text!.remove(at: displayField.text!.index(before: (displayField.text!.endIndex)))
+        if displayField.text!.characters.count < 1 {
+            displayField.text = "0"
+            userWasTyping = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
