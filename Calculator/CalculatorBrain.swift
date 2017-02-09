7b1c3ea8fe1d64c20bf40395cf02db0322b860d5
Grey Patterson <greypatterson@gmail.com>
2017-02-09 10:34:24 -0800

    Start making the brains of the operation

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Grey Patterson on 2017-02-09.
+//  Copyright © 2017 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    func setOperand (operand: Double){ }
+    
+    func performOperation(symbol: String){ }
+    
+    var result: Double{
+        get{
+            return 0 // temporary so Xcode will stop complaining
+        }
+    }
+}
