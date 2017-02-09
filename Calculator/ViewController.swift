55ba754dae58dcf964abac2babcfdf2c71e72bd4
Grey Patterson <greypatterson@gmail.com>
2017-02-09 10:14:25 -0800

    Add a calculated variable for the display field

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,26 +13,46 @@ class ViewController: UIViewController {
     
     var userWasTyping = false
     
+    var displayValue: Double{
+        get {
+            return Double(displayField.text!)!
+        }
+        set {
+            displayField.text = String(newValue)
+        }
+    }
+    
     @IBAction func digitPress(_ sender: UIButton) { // Handles the user pressing a digit
         let digitPressed = sender.title(for: .normal)! // Which digit did they press?
         if !userWasTyping{ // If this is the first digit they're inputting, remove the zero first
             userWasTyping = true
-            displayField.text = digitPressed
+            displayValue = Double(digitPressed)!
         }else{ // Otherwise, just add on the new digit
-            displayField.text = displayField.text! + digitPressed
+            var temp = String(displayValue)
+            temp += digitPressed
+            displayValue = Double(temp)!
+//            displayField.text = displayField.text! + digitPressed
         }
     }
     
     @IBAction func deleteKeyPress(_ sender: UIButton) { // Handles user pressing the delete key
-        let length = displayField.text!.characters.count
+        var temp = displayField.text!
+//        let length = displayField.text!.characters.count
+        let length = temp.characters.count
         guard length > 0 else { // If the length is 0 and you try to continue, it'll mean Problems
             return
         }
-        displayField.text!.remove(at: displayField.text!.index(before: (displayField.text!.endIndex))) // Remove the last character
-        if displayField.text!.characters.count < 1 { // If that was the *only* character that you just removed, reset to the default-zero
-            displayField.text = "0"
-            userWasTyping = false
+        temp.remove(at: temp.index(before: temp.endIndex))
+        if temp.characters.count < 1 {
+            displayValue = 0.0
+        }else{
+            displayValue = Double(temp)!
         }
+//        displayField.text!.remove(at: displayField.text!.index(before: (displayField.text!.endIndex))) // Remove the last character
+//        if displayField.text!.characters.count < 1 { // If that was the *only* character that you just removed, reset to the default-zero
+//            displayField.text = "0"
+//            userWasTyping = false
+//        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
