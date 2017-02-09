e3dc5d823a197b42ae96de2969313f1e66ab15c7
Grey Patterson <greypatterson@gmail.com>
2017-02-09 11:02:04 -0800

    Being fancier about how it works internally

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -15,14 +15,32 @@ class CalculatorBrain{
         accumulator = operand
     }
     
-    var operations: Dictionary<String, Double> = [
-        "π": M_PI,
-        "e": M_E
+    var operations: Dictionary<String, Operation> = [
+        "π": Operation.Constant(M_PI),
+        "e": Operation.Constant(M_E),
+        "√": Operation.UnaryOperation, //sqrt,
+        "cos": Operation.UnaryOperation //cos
     ]
     
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation
+        case BinaryOperation
+        case Equals
+    }
+    
     func performOperation(symbol: String){
-        if let constant = operations[symbol]{
-            accumulator = constant
+        if let operation = operations[symbol]{
+            switch operation{
+                case .Constant(let value):
+                    accumulator = value
+                case .UnaryOperation:
+                    break
+                case .BinaryOperation:
+                    break
+                case .Equals:
+                    break
+            }
         }
     }
     
