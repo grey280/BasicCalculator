b3bee3e04fce86cbf2ee3bc2ff7c5101ec431bb0
Grey Patterson <greypatterson@gmail.com>
2017-02-09 10:47:33 -0800

    Basic operation processing

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -9,17 +9,26 @@
 import Foundation
 
 class CalculatorBrain{
+    private var accumulator = 0.0
+    
     func setOperand (operand: Double){
-        
+        accumulator = operand
     }
     
     func performOperation(symbol: String){
-        
+        switch symbol {
+        case "π":
+            accumulator = M_PI
+        case "√":
+            accumulator = sqrt(accumulator)
+        default:
+            break
+        }
     }
     
     var result: Double{
         get{
-            return 0 // temporary so Xcode will stop complaining
+            return accumulator
         }
     }
 }
